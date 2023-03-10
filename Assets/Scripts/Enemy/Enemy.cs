f3714ecfc3790b1e39a73d6bad3d3e49f2a73621
Tolmak <tankist10rus@gmail.com>
2023-03-11 00:28:54 +0300

    intermediate result - add some feat, fix code. workable, but dont have some feat

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,45 +7,50 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
+    [SerializeField] private ParticleSystem _particalSystem;
 
     // link to the higher mind
     public EnemyManager EnemyManager;
 
+    // link another scripts for get information
+    [SerializeField] private EnemyDefense _enemyDefence;
+
     // field:
     // about health
-    [SerializeField] private float _maxHealth;
+    public float MaxHealth;
     public float CurrentHealth;
     private bool _isAlive = true;
-    [SerializeField] private bool _canDefence;
-    [SerializeField] private bool _haveDefence;
     [SerializeField] private bool _canHealHimself;
     [SerializeField] private bool _canHealAnotherEnemy;
     [SerializeField] private bool _canTransferDamage;
-
-    // influenct on player
-    [SerializeField] private bool _canSlowBall;
 
     // position
     [SerializeField] private bool _canChangePosition;
 
     [SerializeField] private float _abilityTimer;
 
-    public void Initialize(EnemyManager EnemyManager, float maxHealth, bool haveInvul, bool canHealHimself, bool canHealAnotherEnemy, bool canTransferDamage, bool canChangePosition, bool canSlowBall)
+    public void Initialize(EnemyManager EnemyManager, float maxHealth, bool canHealHimself, bool canHealAnotherEnemy, bool canTransferDamage, bool canChangePosition)
     {
         this.EnemyManager = EnemyManager;
 
-        _maxHealth = maxHealth;
-        CurrentHealth = _maxHealth;
-
-        _canDefence = haveInvul;
+        MaxHealth = maxHealth;
+        CurrentHealth = MaxHealth;
 
         _canHealHimself = canHealHimself;
         _canHealAnotherEnemy = canHealAnotherEnemy;
         _canTransferDamage = canTransferDamage;
         _canChangePosition = canChangePosition;
 
-        _canSlowBall = canSlowBall;
+    }
 
+    private void Awake()
+    {
+        _particalSystem.Stop();
+
+        if (EnemyManager == null) 
+        {
+            EnemyManager = GetComponentInParent<EnemyManager>();
+        }
     }
 
     private void Update()
@@ -54,16 +59,10 @@ public class Enemy : MonoBehaviour
         {
             if (CanUseAbility())
             {
-                /*
-                if (CurrentHealth != _maxHealth)
+                if (CurrentHealth != MaxHealth)
                 {
                     Heal(this);
-                }
-                */
-                if (_canDefence && !_haveDefence)
-                {
-                    StartCoroutine(StayInvul());
-                }
+                }   
             }
         }
         else
@@ -77,25 +76,18 @@ public class Enemy : MonoBehaviour
         if (collision.rigidbody.GetComponent<Ball>() is Ball ball)
         {
             TakeDamage(ball.DamageValue, ball.AttackType);
-
-            if (_canSlowBall)
-            {
-                //StartCoroutine(ball.SlowBallForTime(2, 5));
-            }
         }
     }
 
     public void TakeDamage(float damageValue, AttackType AttackType = AttackType.Default)
     {
-        if (AttackType == AttackType.Special || !_haveDefence)
+        if (AttackType == AttackType.Special || !_enemyDefence.GetDefenceState())
         {
             CurrentHealth -= damageValue;
 
             if (_isAlive = CheckDeath())
             {
-                GameplayEventManager.SendEnemyKilled();
-                Destroy(gameObject);
-                //Debug.Log($"{gameObject.name} dead");
+                Death();
             }
         }
     }
@@ -105,12 +97,22 @@ public class Enemy : MonoBehaviour
         return CurrentHealth <= 0 ? true : false;
     }
 
+    private void Death()
+    {
+        GameplayEventManager.SendEnemyKilled();
+        EnemyManager.EnemyCount--;
+        EnemyManager.DeleteEnemyFromList(this);
+        Destroy(gameObject);
+        //Debug.Log($"{gameObject.name} dead");
+    }
+
     public void Heal(Enemy target)
     {
-        if (target.CurrentHealth != target._maxHealth)
+        if (target.CurrentHealth != target.MaxHealth)
         {
-            target.CurrentHealth = target._maxHealth;
+            target.CurrentHealth = target.MaxHealth;
             _abilityTimer = 0f;
+            target._particalSystem.Play();
         }
     }
 
@@ -122,19 +124,6 @@ public class Enemy : MonoBehaviour
 
     private bool CanUseAbility()
     {
-        return Random.Range(0, 100) > 60 ? true : false;
-    }
-
-    private IEnumerator StayInvul()
-    {
-        _haveDefence = true;
-        _renderer.material.color = Color.white;
-
-        _abilityTimer = 0f;
-
-        yield return new WaitForSeconds(5);
-
-        _haveDefence = false;
-        _renderer.material.color = Color.red;
+        return Random.Range(0, 100) <= 60 ? true : false;
     }
 }
