e1fe43f222cc66069d6fd52669f711be33c8be11
Alexandr Malkov <a.malkov.113@gmail.com>
2023-03-10 13:33:55 +0300

    Add noMoveEnemy and AbilityIcon prefab

---
+++ Assets/Scripts/Ball/NoMoveEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoMoveEnemy : MonoBehaviour
+{
+    [SerializeField] private float _timeToNextCast;
+    [SerializeField] private float _timeOfAction;
+    [SerializeField] private ChargeIcon _noMoveEnemyChargeIcon;
+
+    private EnemyManager _enemyManager;
+    private float _timer;
+    void Start()
+    {
+        _enemyManager = FindAnyObjectByType<EnemyManager>();
+        _timer = _timeToNextCast;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && _timer > _timeToNextCast)
+        {
+            _enemyManager.StopEnemyMoving(_timeOfAction);
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
+        _noMoveEnemyChargeIcon.SetChargeValue(_timer, _timeToNextCast);
+    }
+}
