3752790ef4ed4281b827c9d5d231401038fdff87
Alexandr Malkov <a.malkov.113@gmail.com>
2023-03-10 15:40:06 +0300

    add clone balls to prefab, create gameplayEventController

---
+++ Assets/Scripts/GameScripts/GameplayEventManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameplayEventManager : MonoBehaviour
+{
+    public static UnityEvent OnEnemyKilled = new UnityEvent();
+    
+    public static void SendEnemyKilled()
+    {
+        OnEnemyKilled.Invoke();
+    }
+}
