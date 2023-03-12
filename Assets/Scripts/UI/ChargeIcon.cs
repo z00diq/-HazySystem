452df0b1f7fec3294d735dfc1d94b86f9295aee9
Alexandr Malkov <a.malkov.113@gmail.com>
2023-03-12 18:00:21 +0300

    Change texture in AbitiryUI

--- Assets/Scripts/UI/ChargeIcon.cs
+++ Assets/Scripts/UI/ChargeIcon.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChargeIcon : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _timer;
+    
     private Image _background;
     private void Awake()
     {
@@ -12,7 +15,18 @@ public class ChargeIcon : MonoBehaviour
     }
     public void SetChargeValue(float currentCharge, float maxCharge)
     {
-        _background.fillAmount = currentCharge / maxCharge;
+        //_background.fillAmount = currentCharge / maxCharge;
+        if (currentCharge < maxCharge)
+        {
+            _timer.enabled = true;
+            _timer.text = Mathf.Ceil(maxCharge - currentCharge).ToString();
+            _background.color = new Color(1,1,1,0.6f);
+        }
+        else
+        {
+            _timer.enabled = false;
+            _background.color = new Color(1, 1, 1, 1);
+        }
     }
     
 } 
